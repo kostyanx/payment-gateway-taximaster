7842efcd3e923f47e50105222f836075cbb24d15
Konstantin Melnikov <k.melnikov@tinkoff.ru>
2021-01-07 06:05:36 +0300

    init

---
+++ src/main/kotlin/ru/catcab/taximaster/paymentgateway/Application.kt
@@ -0,0 +1,12 @@
+package ru.catcab.taximaster.paymentgateway
+
+
+
+class Application {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello ${args.toList()}")
+        }
+    }
+}
