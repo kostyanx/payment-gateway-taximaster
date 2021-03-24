290f83a0a5d96c90a5cfead108f5568a19ee72b4
Konstantin Melnikov <melkonnik@gmail.com>
2021-03-24 14:15:25 +0300

    up version to 1.2.0

--- src/dist/run.bat
+++ src/dist/run.bat
@@ -1,3 +1,3 @@
 @echo on
 
-java -Xmx512m -Dlog4j.configurationFile="log4j2.xml" -Dfile.encoding=UTF-8 -Dlog4j.console.appender=stdout_win -jar payment-gateway-taximaster-1.1.1.jar -config=application.conf %*
+java -Xmx512m -Dlog4j.configurationFile="log4j2.xml" -Dfile.encoding=UTF-8 -Dlog4j.console.appender=stdout_win -jar payment-gateway-taximaster-1.2.0.jar -config=application.conf %*
